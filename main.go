b0f8dbf744ba58a76cbfdb46914caee8a0d36ff0
Ivan Petrukhin <petruekhin@yandex.ru>
2025-02-09 21:52:19 +0300

    Move configuration of api host to env API_HOST

--- main.go
+++ main.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"os"
 	openapi "pinger/api"
 	"sync"
 	"time"
 
+	"log"
+
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 	probing "github.com/prometheus-community/pro-bing"
-	"log"
 )
 
 func main() {
+	apiHost, ok := os.LookupEnv("API_HOST")
+	if !ok {
+		panic(fmt.Errorf("specify api host in environment variable API_HOST"))
+	}
+
 	apiClient, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
 		panic(err)
@@ -26,7 +34,7 @@ func main() {
 
 	ctx := context.Background()
 
-	sw := openapi.NewAPIClient(&openapi.Configuration{Scheme: "http", Host: "localhost:3001", Servers: []openapi.ServerConfiguration{{}}})
+	sw := openapi.NewAPIClient(&openapi.Configuration{Scheme: "http", Host: apiHost, Servers: []openapi.ServerConfiguration{{}}})
 	wg := &sync.WaitGroup{}
 
 	for _, ctr := range containers {
